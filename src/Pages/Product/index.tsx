e0af707c9702eaa0044575593d1b4f6f33d6391e
Alber <kovalevskij.albert@gmail.com>
2022-10-05 19:25:38 +0300

    initial

---
+++ src/Pages/Product/index.tsx
@@ -0,0 +1,42 @@
+import { useParams } from "react-router-dom"
+import { ButtonCTA } from "../../Components/ButtonCTA"
+import { Rating } from "../../Components/Rating"
+import { ItemInterface, PageProps } from "../../globalTypes"
+
+export const Product: React.FC<PageProps> = ({ state, dispatch }): JSX.Element => {
+  const { title } = useParams()
+  const { items } = state
+  const item: ItemInterface = items.find(index => index.title.trim() === title?.trim()) as ItemInterface
+
+  // useEffect(() => {
+  //   window.scrollTo(0, 0)
+  // }, [searching])
+
+  return(
+    <section className="Detail">
+      <article className="Detail__thumbnail">
+        <img src={item.image} alt="" />
+      </article>
+
+      <article className="Detail__info">
+        <div className="Detail__info--header">
+          <h2>{item.title}</h2>
+          <ButtonCTA
+            ItemId={item.id}
+            dispatch={dispatch}
+            added={item.added}
+          />
+        </div>
+
+        <div className="Detail__info--meta">
+          <span className="Detail__price">${item.price}</span>
+          <Rating content={item.rating.rate} />
+        </div>
+
+        <p className="Detail__info--description">
+          {item.description}
+        </p>
+      </article>
+    </section>
+  )
+}
