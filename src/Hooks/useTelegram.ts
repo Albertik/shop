5286b7ecced4013b0638a3ec0dc803add0af7f74
Alber <kovalevskij.albert@gmail.com>
2022-10-14 21:13:49 +0300

    commit

--- src/Hooks/useTelegram.ts
+++ src/Hooks/useTelegram.ts
@@ -1,4 +1,4 @@
-const tg = window.Telegram.WebApp;
+const tg = (window as any).Telegram.WebApp;
 
 export function useTelegram() {
 	const onClose = () => {
---
+++ src/Pages/Form/index.tsx
@@ -0,0 +1,66 @@
+import { useCallback, useEffect, useState } from 'react';
+import './Form.scss';
+import { useTelegram } from '../../Hooks/useTelegram';
+
+const Form = () => {
+	const [country, setCountry] = useState('');
+	const [street, setStreet] = useState('');
+	const [subject, setSubject] = useState('physical');
+	const { tg } = useTelegram();
+
+	const onSendData = useCallback(() => {
+		const data = {
+			country,
+			street,
+			subject,
+		};
+		tg.sendData(JSON.stringify(data));
+	}, [country, street, subject, tg]);
+
+	useEffect(() => {
+		tg.onEvent('mainButtonClicked', onSendData);
+		return () => {
+			tg.offEvent('mainButtonClicked', onSendData);
+		};
+	}, [onSendData, tg]);
+
+	useEffect(() => {
+		tg.MainButton.setParams({
+			text: 'Отправить данные',
+		});
+	}, [tg.MainButton]);
+
+	useEffect(() => {
+		if (!street || !country) {
+			tg.MainButton.hide();
+		} else {
+			tg.MainButton.show();
+		}
+	}, [country, street, tg.MainButton]);
+
+	const onChangeCountry = (e: any) => {
+		setCountry(e.target.value);
+	};
+
+	const onChangeStreet = (e: any) => {
+		setStreet(e.target.value);
+	};
+
+	const onChangeSubject = (e: any) => {
+		setSubject(e.target.value);
+	};
+
+	return (
+		<div className={'form'}>
+			<h3>Введите ваши данные</h3>
+			<input className={'input'} type='text' placeholder={'Страна'} value={country} onChange={onChangeCountry} />
+			<input className={'input'} type='text' placeholder={'Улица'} value={street} onChange={onChangeStreet} />
+			<select value={subject} onChange={onChangeSubject} className={'select'}>
+				<option value={'physical'}>Физ. лицо</option>
+				<option value={'legal'}>Юр. лицо</option>
+			</select>
+		</div>
+	);
+};
+
+export default Form;
